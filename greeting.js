921e16186efcd35f782176ec894ae8f26bc9393b
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-04 20:19:17 -0800

    test completed with checking if arg is processed through terminal

--- greeting.js
+++ greeting.js
@@ -1,6 +1,6 @@
-var greeting = {
-  greet: function (name){
-    return 'Hello '+ name;
-  }
-};
-module.exports = greeting;
+var greeting = require('./printGreeting');
+if(process.argv.length === 3){
+  var output = greeting.greet(process.argv[2]);
+  console.log(output);
+}
+
